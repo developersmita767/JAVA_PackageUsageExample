d5dc812ec127ea68fa07558634cd380526751851
developersmita767 <156692415+developersmita767@users.noreply.github.com>
2024-07-08 00:28:06 +0530

    Add files via upload

---
+++ PackageUsage.java
@@ -0,0 +1,7 @@
+import mypack.PackageExample;
+public class PackageUsage{
+    public static void main(String[] ar){
+        PackageExample p =new PackageExample();
+        p.m1("Hello world");
+    }
+}
